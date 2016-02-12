60ccf209eaf9b1a947f9487098fa06d32b8a2b81
Ahmed Ashraf <Ahmed Ashraf@DESKTOP-JNGM71A>
2016-02-12 22:39:54 +0200

    Version 3.3  friend request part 2

--- ChattingAppServer/src/controller/ServerFormController.java
+++ ChattingAppServer/src/controller/ServerFormController.java
@@ -1,6 +1,5 @@
 package controller;
 
-import controller.Controller;
 import java.net.URL;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
@@ -13,7 +12,6 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.chart.PieChart;
 import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
@@ -24,8 +22,7 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import model.pojo.User;
-import view.Validation;
-import static view.Validation.nameValidation;
+import static controller.Validation.nameValidation;
 
 public class ServerFormController implements Initializable {
     
